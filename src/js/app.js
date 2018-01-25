687be64d3f0081504950a2eb6ffbb3c8247e406d
Suelen Braga <suelen.braga2006@gmail.com>
2018-01-25 19:26:34 -0200

    Mobile first ok

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+$(function(){
+	$('html, body').on('click', function(e){
+		if(e.target == document.documentElement){
+			$('html').removeClass('open-sidebar');
+		}
+	});
+
+	$('.js-open-sidebar').on('click', function(){
+		$('html').addClass('open-sidebar');
+	});
+});
